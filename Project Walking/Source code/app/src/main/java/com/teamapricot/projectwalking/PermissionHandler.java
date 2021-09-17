982de0b7daf8bb18b5356f847849d43d7ccd0ba5
Erik Wahlberger <EWahlberger@gmail.com>
2021-09-17 09:56:38 +0200

    Add methods to check for granted permissions
    Helper method added to easily check for a single permission, as well as the regular method for checking for multiple permissions

--- Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/PermissionHandler.java
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/PermissionHandler.java
@@ -1,5 +1,10 @@
 package com.teamapricot.projectwalking;
 
+import android.content.pm.PackageManager;
+import androidx.core.content.ContextCompat;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Erik Wahlberger
@@ -10,4 +15,39 @@ package com.teamapricot.projectwalking;
  * Make sure to add each permission to the AndroidManifest.xml using {@literal <uses-permission android:name="..." />}.
  */
 public class PermissionHandler {
+    /**
+     * Checks if multiple permissions have already been granted.
+     * @param permissions The permissions to be checked. Can be found within PackageManager.permissions
+     * @return A {@code Map<String, Boolean> }. The keys ({@code String}) represents each permission. The values ({@code Boolean}) represent the current permission status for specific permissions.
+     * To check if a permission has already been granted or not, use the {@code Map} return value with the searched permission as the key.
+     */
+    public Map<String, Boolean> checkPermissions(String[] permissions) {
+        Map<String, Boolean> permissionResults = new HashMap<>();
+
+        for (String permission : permissions) {
+            if (permissionResults.keySet().contains(permission)) {
+                continue;
+            }
+
+            Boolean isGranted = ContextCompat.checkSelfPermission(this.activity, permission) == PackageManager.PERMISSION_GRANTED;
+            permissionResults.put(permission, isGranted);
+        }
+
+        return permissionResults;
+    }
+
+    /**
+     * Checks if a permission has already been granted.
+     * @param permission The permission to be checked. Can be found within PackageManager.permissions
+     * @return A {@code boolean} value representing whether or not the permission has already been granted.
+     */
+    public boolean checkPermission(String permission) {
+        Map<String, Boolean> permissionResults = checkPermissions(new String[]{permission});
+
+        if (permissionResults.keySet().contains(permission)) {
+            return permissionResults.get(permission);
+        }
+
+        return false;
+    }
 }
