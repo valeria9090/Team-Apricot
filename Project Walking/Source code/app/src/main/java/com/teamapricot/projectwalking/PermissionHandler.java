52fa7ff9fd7042a1603e6244a621054a5a8b1418
Erik Wahlberger <EWahlberger@gmail.com>
2021-09-17 09:37:18 +0200

    Add PermissionHandler class
    Used to simplify checking and requesting permissions for Android applications

---
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/PermissionHandler.java
@@ -0,0 +1,13 @@
+package com.teamapricot.projectwalking;
+
+
+/**
+ * @author Erik Wahlberger
+ * @version 2021-09-17
+ *
+ * PermissionHandler is used to check for already granted permissions, as well as
+ * request new permissions in an easier way than by using the standard implementation by Google.
+ * Make sure to add each permission to the AndroidManifest.xml using {@literal <uses-permission android:name="..." />}.
+ */
+public class PermissionHandler {
+}
