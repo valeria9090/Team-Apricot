25d66231dcf0c80ddb117689ba068f2097970ae0
Erik Wahlberger <EWahlberger@gmail.com>
2021-09-17 10:06:18 +0200

    Add methods to request permissions
    Add a helper method for requesting single permissions, as well as the regular method for requesting multiple permissions. Bumping the minSdk to 24 (Nougat) since CompletableFuture is not available for previous Sdk versions. Might revisit the implementation later to allow for older Sdks if that's desirable.

--- Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/PermissionHandler.java
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/PermissionHandler.java
@@ -1,10 +1,17 @@
 package com.teamapricot.projectwalking;
 
 import android.content.pm.PackageManager;
+import android.os.SystemClock;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author Erik Wahlberger
@@ -15,6 +22,33 @@ import java.util.Map;
  * Make sure to add each permission to the AndroidManifest.xml using {@literal <uses-permission android:name="..." />}.
  */
 public class PermissionHandler {
+    private final String TAG = "PermissionHandler";
+
+    private ActivityResultLauncher<String> requestPermissionLauncher;
+    private Boolean currentPermissionResult;
+    private ReentrantLock currentPermissionResultLock;
+    private AppCompatActivity activity;
+
+    /**
+     * Creates a new permission handler for the specified AppCompatActivity.
+     * @param activity The activity where the permission handler will be used
+     */
+    public PermissionHandler(AppCompatActivity activity) {
+        currentPermissionResult = null;
+        currentPermissionResultLock = new ReentrantLock();
+        this.activity = activity;
+
+        requestPermissionLauncher = activity.registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            // Set currentPermissionResult when a permission has been granted or denied
+            try {
+                currentPermissionResultLock.tryLock();
+                currentPermissionResult = isGranted;
+            } finally {
+                currentPermissionResultLock.unlock();
+            }
+        });
+    }
+
     /**
      * Checks if multiple permissions have already been granted.
      * @param permissions The permissions to be checked. Can be found within PackageManager.permissions
@@ -49,5 +83,68 @@ public class PermissionHandler {
         }
 
         return false;
+    }
+
+    /**
+     * Request a single permission asynchronously.
+     * @param permissions The permissions to be requested. Can be found within PackageManager.permissions
+     * @return A {@code CompletableFuture} object containing a {@code Map<String, Boolean>}.
+     * Each key ({@code String}) represents a certain permission. Each value ({@code Boolean}) shows if a particular permission was granted or rejected.
+     * To check if a permission was granted or not, use the map return value with the searched permission as the key.
+     */
+    public CompletableFuture<Map<String, Boolean>> requestPermissionsAsync(String[] permissions) {
+        return CompletableFuture.supplyAsync(() -> {
+            Map<String, Boolean> permissionResults = new HashMap<>();
+
+            for (String permission : permissions) {
+                // Launch the permission launcher on the UI thread, otherwise the permission dialog is not shown on the screen
+                this.activity.runOnUiThread(() -> {
+                    requestPermissionLauncher.launch(permission);
+                });
+
+                waitForPermissionResult();
+
+                currentPermissionResultLock.tryLock();
+                permissionResults.put(permission, currentPermissionResult);
+                
+                currentPermissionResult = null;
+                currentPermissionResultLock.unlock();
+            }
+
+            return permissionResults;
+        });
+    }
+
+    /**
+     * Request a single permission asynchronously.
+     * @param permission The permission to be requested. Can be found within PackageManager.permissions
+     * @return A {@code CompletableFuture} object containing a {@code Boolean} value that shows if the permission request was granted or rejected
+     */
+    public CompletableFuture<Boolean> requestPermissionAsync(String permission) {
+        return requestPermissionsAsync(new String[]{permission}).thenApply(permissionResults -> {
+            if (permissionResults.keySet().contains(permission)) {
+                return permissionResults.get(permission);
+            }
+
+            return false;
+        });
+    }
+
+    /**
+     * Wait for the callback to requestPermissionLauncher to be triggered
+     */
+    private void waitForPermissionResult() {
+        while(true) {
+            try {
+                currentPermissionResultLock.tryLock();
+                if (currentPermissionResult != null) {
+                    break;
+                }
+            } finally {
+                currentPermissionResultLock.unlock();
+            }
+
+            SystemClock.sleep(500);
+        }
     }
 }
