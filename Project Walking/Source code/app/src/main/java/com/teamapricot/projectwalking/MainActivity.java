c3b81dd5f60b722a59e4ce02907dba982fe8162d
valeria9090 <74568062+valeria9090@users.noreply.github.com>
2021-09-21 10:56:35 +0200

    Update MainActivity.java

--- Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/MainActivity.java
+++ Project Walking/Source code/app/src/main/java/com/teamapricot/projectwalking/MainActivity.java
@@ -1,14 +1,42 @@
 package com.teamapricot.projectwalking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button notifyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notifyBtn = findViewById(R.id.notify_button);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel= new NotificationChannel("notify_btn","notify_btn", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager=getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        notifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "notify_btn");
+                builder.setContentTitle("New Spot");
+                builder.setContentText("please find me");
+                builder.setSmallIcon(android.R.drawable.arrow_down_float);
+                builder.setAutoCancel(true);
+                NotificationManagerCompat managerCompat;
+                managerCompat = NotificationManagerCompat.from(MainActivity.this);
+                managerCompat.notify(1, builder.build());
+
+            }
+        });
     }
 }
